c8a561f69d9135796ea6710aff4b9029f7f11a9b
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2021-09-02 04:43:00 +0530

    Update Cannon.js

--- js/Cannon.js
+++ js/Cannon.js
@@ -5,26 +5,27 @@ class Cannon {
     this.width = width;
     this.height = height;
     this.angle = angle;
-    this.cannon_image = loadImage("assets/CANON.png");
-    this.cannon_base = loadImage("assets/cannon_base.png");
+    this.cannon_image = loadImage("assets/canon.png");
+    this.cannon_base = loadImage("assets/cannonBase.png");
   }
   display() {
-    if (keyIsDown(RIGHT_ARROW) && this.angle < 0.1) {
-      this.angle += 0.01;
+    if (keyIsDown(RIGHT_ARROW) && this.angle<70  ) {
+      this.angle += 1;
     }
 
-    if (keyIsDown(LEFT_ARROW) && this.angle > -1.45) {
-      this.angle -= 0.01;
+    if (keyIsDown(LEFT_ARROW) && this.angle>-30 ) {
+      this.angle -= 1;
     }
 
-    fill("#676e6a");
+
     push();
     translate(this.x, this.y);
     rotate(this.angle);
-    imageMode(CENTER)
+    imageMode(CENTER);
     image(this.cannon_image, 0, 0, this.width, this.height);
     pop();
-    image(this.cannon_base, 70, 20, 200, 200, PI, TWO_PI);
+    image(this.cannon_base, 70, 20, 200, 200);
     noFill();
   }
 }
+
